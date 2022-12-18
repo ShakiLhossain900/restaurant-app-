7990c85c9895aa2d59d2cb7b0d84d19cec60f805
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-12-18 18:36:55 +0600

    adding the

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -33,8 +33,7 @@ const HomeContainer = () => {
         <p className="text-base text-textColor text-center md:text-left md:w-[80%]">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima velit
           eaque fugit distinctio est nam voluptatum architecto, porro iusto
-          deserunt recusandae ipsa minus eos sunt, dolores illo repellat facere
-          suscipit!
+         
         </p>
 
         <button
